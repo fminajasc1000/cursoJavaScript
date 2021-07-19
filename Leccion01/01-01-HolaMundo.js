a4c1bafb13a50d66d749bc228bdf203d2bc70ac1
franciscominajas <franciscominajas@gmail.com>
2021-07-18 21:07:43 -0500

    first commit

---
+++ Leccion01/01-01-HolaMundo.js
@@ -0,0 +1,7 @@
+/**
+ * Ejemplos de tipos de datos en javascript
+ */
+
+//tipo de dato string
+var nombre = "Hola Mundo desde JavaScript";
+console.log(nombre);
---
+++ Leccion01/Prueba01-01.js
@@ -0,0 +1,3 @@
+var nombre = "Danae";
+
+console.log(nombre);
