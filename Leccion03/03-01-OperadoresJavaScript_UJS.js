d34f27ce55b6a53b2fb76a86cd9aacde88c1ca17
franciscominajas <franciscominajas@gmail.com>
2021-07-18 21:32:06 -0500

    operadores + - * / **

---
+++ Leccion03/03-01-OperadoresJavaScript_UJS.js
@@ -0,0 +1,18 @@
+let a = 3, b=2;
+let z = a+b;
+console.log("Resultado e la suma: "+z);
+
+z = a - b;
+console.log("Resulatado de la resta: "+z)
+
+z = a * b;
+console.log("Resulatado de la multiplicacion: "+z);
+
+z = a / b;
+console.log("Resulatado de la division: "+z);
+
+z = a % b;
+console.log("Resulatado del residuo: "+z);
+
+z = a ** b;
+console.log("Resulatado del operador exponente: "+z);
