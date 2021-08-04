20f942e997eb2e9bca48417ebc884d0562f65ab9
francisco.minajas <francisco.minajas@banregio.com>
2021-08-04 15:32:55 -0500

    operadores logicos

--- Leccion03/03-01-OperadoresJavaScript_UJS5.js
+++ Leccion03/03-01-OperadoresJavaScript_UJS5.js
@@ -1,3 +1,4 @@
+//ejemplo de and, regresa true si ambos son verdaderos
 let a = 5;
 let valMin = 0;
 let valMax = 10;
@@ -6,4 +7,12 @@ if( a>=valMin && a<=valMax){
     console.log("Dentro de rango");
 }else{
     console.log("Fuera de rango");
+}
+
+//ejemplo de or, regresa true si cualquiera de los operandos es true
+let vacaciones = false, diaDescanso = true;
+if(vacaciones || diaDescanso){
+    console.log("Padre puede asistir al juego del hijo");
+}else{
+    console.log("Padre ocupado");
 }
