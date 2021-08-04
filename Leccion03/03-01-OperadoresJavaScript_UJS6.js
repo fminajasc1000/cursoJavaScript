3e517af5a1a969accbe500c39be287de1983a16e
francisco.minajas <francisco.minajas@banregio.com>
2021-08-04 16:10:36 -0500

    operador ternario e ifs

--- Leccion03/03-01-OperadoresJavaScript_UJS6.js
+++ Leccion03/03-01-OperadoresJavaScript_UJS6.js
@@ -3,13 +3,18 @@ let miNumero = "19";
 //console.log(typeof miNumero);
 
 let edad = Number(miNumero);
+console.log(edad);
 console.log(typeof edad);
 
-if(edad>=18){
-    console.log("Puede votar");
+if(isNaN(edad)){
+    console.log("No es un numero");
 }else{
-    console.log("Muy joven para votar");
+    if(edad>=18){
+        console.log("Puede votar");
+    }else{
+        console.log("Muy joven para votar");
+    }
+    
+    let resultado = (edad>=18)?"Puede votar":"Muy joven para votar";
+    console.log(resultado);
 }
-
-let resultado = (edad>=18)?"Puede votar":"Muy joven para votar";
-console.log(resultado);
