643970d19505b9e4dae2a6e4b09303181b18fc3d
francisco.minajas <francisco.minajas@banregio.com>
2021-08-04 17:44:28 -0500

    condiciones if3

--- Leccion04/04-01-SentenciasControl_UJS.js
+++ Leccion04/04-01-SentenciasControl_UJS.js
@@ -11,3 +11,39 @@ if( numero == 1){
 }else{
     console.log("Numero desconocido");
 }
+
+//------------
+let mes = 12;
+let estacion;
+
+if( mes == 1 || mes == 2 || mes == 12){
+    estacion = "Invierno";
+}else if(mes == 3 || mes == 4 || mes == 5){
+    estacion = "Primavera";
+}else if(mes == 6 || mes == 7 || mes == 8){
+    estacion == "Verano";
+}else if(mes == 9 || mes == 10 || mes == 11){
+    estacion == "Otoño";
+}else{
+    estacion = "Valor incorrecto";
+}
+
+console.log(estacion);
+
+//-----
+let hora = 19;
+let estado;
+
+if( hora >= 6 && hora <= 11){
+    estado = "Buenos dias";
+}else if(hora >= 12 && hora <= 18){
+    estado = "Buenas tardes";
+}else if(hora >= 19 && hora <= 24){
+    estado == "Buenas noches";
+}else if(hora >= 0 && hora <= 6){
+    estado == "Durmiendo";
+}else{
+    estado = "Valor incorrecto";
+}
+
+console.log(estado);
